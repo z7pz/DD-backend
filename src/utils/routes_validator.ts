8e1314e4e113f7fdb1dd53d69a54f2cd296ee129
z7pz <sdfygamer@gmail.com>
2022-09-21 12:59:36 +0300

    fix(regex): add more methods for route expression

--- src/utils/routes_validator.ts
+++ src/utils/routes_validator.ts
@@ -1,5 +1,5 @@
 export function route_validator(name: string) {
-  const exp = /(GET|POST)\s?(\/?.+)/;
+  const exp = /(GET|POST|DELETE|PATCH|PUT)\s?(\/?.+)/;
   const matchs = name.match(exp);
   if (!matchs || !exp.test(name)) throw new Error("Invalid Route: " + name);
   let [, method, route] = matchs;
